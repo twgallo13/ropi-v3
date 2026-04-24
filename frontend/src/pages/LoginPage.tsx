40dc67c6a066790ea553e7ba2a4d582a873562a0
twgallo13 <theo@shiekhshoes.org>
2026-04-24 07:33:20 +0000

    chore: add autoComplete attribute to login password input

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -81,6 +81,7 @@ export default function LoginPage() {
           <input
             type="password"
             required
+            autoComplete="current-password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
